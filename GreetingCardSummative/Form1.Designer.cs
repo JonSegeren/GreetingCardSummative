482504feaee48212d7d8e851cfa6daf486a39687
JonSegeren <jonasege101@ed.amdsb.ca>
2017-09-21 12:57:49 -0400

    Add project files.

---
+++ GreetingCardSummative/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace GreetingCardSummative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.StartButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("StartButton.BackgroundImage")));
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.ForeColor = System.Drawing.Color.PaleGoldenrod;
+            this.StartButton.Location = new System.Drawing.Point(-2, -10);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(456, 466);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Click to Start";
+            this.StartButton.UseVisualStyleBackColor = false;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(454, 454);
+            this.Controls.Add(this.StartButton);
+            this.Name = "Form1";
+            this.Text = "Wilkommen, Bienvinue, Welcome";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ GreetingCardSummative/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace GreetingCardSummative
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            //All the 'fun' beginning stuff (sept 20)
+            SoundPlayer DING = new SoundPlayer(Properties.Resources.Elevator_Ding_SoundBible_com_685385892);
+            Graphics orionGraphics = this.CreateGraphics();
+            SolidBrush starBrush = new SolidBrush(Color.White);
+            Pen starPen = new Pen(Color.PaleGoldenrod, 3);
+            Font titleFont = new Font("Arial", 36, FontStyle.Bold);
+            SolidBrush wordBrush = new SolidBrush(Color.White);
+
+            //title page (sept21)
+            StartButton.Visible = false;
+
+            //Star Stuff starting now (sept21)
+
+
+
+
+        }
+
+       
+    }
+}
