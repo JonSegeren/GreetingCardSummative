00d41cd92feba61560e95b41c743483e8075936a
JonSegeren <jonasege101@ed.amdsb.ca>
2017-09-22 12:49:06 -0400

    initial commit//more stars added

--- GreetingCardSummative/Form1.cs
+++ GreetingCardSummative/Form1.cs
@@ -31,14 +31,114 @@ namespace GreetingCardSummative
 
             //title page (sept21)
             StartButton.Visible = false;
+            Thread.Sleep(133);
+            this.Refresh();
 
-            //Star Stuff starting now (sept21)
+            //Star Stuff starting now (sept21), constellation
+
+            DING.Play();
+            orionGraphics.FillEllipse(starBrush, 91, 94, 5, 5);
+            Thread.Sleep(1333);
+            orionGraphics.Clear(Color.Black);
+
+            DING.Play();
+            orionGraphics.FillEllipse(starBrush, 91, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 121, 94, 5, 5);
+            Thread.Sleep(1333);
+            orionGraphics.Clear(Color.Black);
+
+            DING.Play();
+            orionGraphics.FillEllipse(starBrush, 91, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 121, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 80, 122, 3, 3);
+            Thread.Sleep(600);
+            orionGraphics.Clear(Color.Black);
+
+            DING.Play();
+            orionGraphics.FillEllipse(starBrush, 91, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 121, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 80, 122, 3, 3);
+            orionGraphics.FillEllipse(starBrush, 117, 244, 7, 7);
+            Thread.Sleep(300);
+            orionGraphics.Clear(Color.Black);
+
+            DING.Play();
+            orionGraphics.FillEllipse(starBrush, 91, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 121, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 80, 122, 3, 3);
+            orionGraphics.FillEllipse(starBrush, 117, 244, 7, 7);
+            orionGraphics.FillEllipse(starBrush, 182, 188, 5, 5);
+            Thread.Sleep(300);
+            orionGraphics.Clear(Color.Black);
+
+            DING.Play();
+            orionGraphics.FillEllipse(starBrush, 91, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 121, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 80, 122, 3, 3);
+            orionGraphics.FillEllipse(starBrush, 117, 244, 7, 7);
+            orionGraphics.FillEllipse(starBrush, 182, 188, 5, 5);
+            orionGraphics.FillEllipse(starBrush,216, 237, 8, 8);
+            Thread.Sleep(300);
+            orionGraphics.Clear(Color.Black);
+
+            DING.Play();
+            orionGraphics.FillEllipse(starBrush, 91, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 121, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 80, 122, 3, 3);
+            orionGraphics.FillEllipse(starBrush, 117, 244, 7, 7);
+            orionGraphics.FillEllipse(starBrush, 182, 188, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 216, 237, 8, 8);
+            orionGraphics.FillEllipse(starBrush, 163, 349, 4, 4);
+            Thread.Sleep(300);
+            orionGraphics.Clear(Color.Black);
+
+            DING.Play();
+            orionGraphics.FillEllipse(starBrush, 91, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 121, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 80, 122, 3, 3);
+            orionGraphics.FillEllipse(starBrush, 117, 244, 7, 7);
+            orionGraphics.FillEllipse(starBrush, 182, 188, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 216, 237, 8, 8);
+            orionGraphics.FillEllipse(starBrush, 163, 349, 4, 4);
+            orionGraphics.FillEllipse(starBrush, 185, 388, 8, 8);
+            Thread.Sleep(300);
+            orionGraphics.Clear(Color.Black);
+
+            DING.Play();
+            orionGraphics.FillEllipse(starBrush, 91, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 121, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 80, 122, 3, 3);
+            orionGraphics.FillEllipse(starBrush, 117, 244, 7, 7);
+            orionGraphics.FillEllipse(starBrush, 182, 188, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 216, 237, 8, 8);
+            orionGraphics.FillEllipse(starBrush, 163, 349, 4, 4);
+            orionGraphics.FillEllipse(starBrush, 185, 388, 8, 8);
+            orionGraphics.FillEllipse(starBrush, 193, 329, 4, 4);
+            Thread.Sleep(300);
+            orionGraphics.Clear(Color.Black);
+
+            DING.Play();
+            orionGraphics.FillEllipse(starBrush, 91, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 121, 94, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 80, 122, 3, 3);
+            orionGraphics.FillEllipse(starBrush, 117, 244, 7, 7);
+            orionGraphics.FillEllipse(starBrush, 182, 188, 5, 5);
+            orionGraphics.FillEllipse(starBrush, 216, 237, 8, 8);
+            orionGraphics.FillEllipse(starBrush, 163, 349, 4, 4);
+            orionGraphics.FillEllipse(starBrush, 185, 388, 8, 8);
+            orionGraphics.FillEllipse(starBrush, 193, 329, 4, 4);
+            orionGraphics.FillEllipse(starBrush, 335, 288, 6, 6);
+            Thread.Sleep(300);
+            orionGraphics.Clear(Color.Black);
+
+
+
 
 
 
 
         }
 
-       
+
     }
 }
